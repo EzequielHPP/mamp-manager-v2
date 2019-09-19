b3212933283b1f7113a7af57821d30c9f0bdf78c
Ezequiel Pereira <info@ezequielhpp.net>
2019-09-19 19:15:47 +0100

    Removed mamp-manager from showing in the hosts

--- resources/views/project-card.blade.php
+++ resources/views/project-card.blade.php
@@ -1,19 +1,21 @@
-<div>
-    <div class="uk-card uk-card-default uk-card-body uk-box-shadow-small uk-box-shadow-hover-large uk-padding">
-        <img src="https://picsum.photos/200/200?grayscale?random={{$index}}" class="uk-border-circle">
-        <h3 class="uk-card-title">{{$project->title}}</h3>
-        <hr class="uk-divider-icon">
-        <div>
-            <a class="uk-button uk-button-default"
-               href="http{{$project->settings->https ? 's':''}}://{{$project->settings->url}}"
-               target="_blank" uk-icon="push"></a>
-            <a class="uk-button uk-button-default" href="#project-settings-{{$project->id}}" uk-toggle><span
-                        uk-icon="icon: cog"></span></a>
-            <a class="uk-button uk-button-danger" href="#project-delete-{{$project->id}}" uk-toggle><span
-                        uk-icon="trash"></span></a>
+@if($project->settings->url !== env('APP_URL'))
+    <div>
+        <div class="uk-card uk-card-default uk-card-body uk-box-shadow-small uk-box-shadow-hover-large uk-padding">
+            <img src="https://picsum.photos/200/200?grayscale?random={{$index}}" class="uk-border-circle">
+            <h3 class="uk-card-title">{{$project->title}}</h3>
+            <hr class="uk-divider-icon">
+            <div>
+                <a class="uk-button uk-button-default"
+                   href="http{{$project->settings->https ? 's':''}}://{{$project->settings->url}}"
+                   target="_blank" uk-icon="push"></a>
+                <a class="uk-button uk-button-default" href="#project-settings-{{$project->id}}" uk-toggle><span
+                            uk-icon="icon: cog"></span></a>
+                <a class="uk-button uk-button-danger" href="#project-delete-{{$project->id}}" uk-toggle><span
+                            uk-icon="trash"></span></a>
+            </div>
         </div>
-    </div>
 
-    @include('modals.project-settings',['project'=>$project])
-    @include('modals.project-delete',['project'=>$project])
-</div>
+        @include('modals.project-settings',['project'=>$project])
+        @include('modals.project-delete',['project'=>$project])
+    </div>
+@endif
