623228276a9077bff11fa2b9bb67376d019f7afd
Ezequiel Pereira <info@ezequielhpp.net>
2019-09-26 13:08:59 +0100

    Removed route checker

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'DashboardController@dashboard')
+     ->name('dashboard');
 
-Route::group(['before' => 'checkUrl'], function () {
-    Route::get('/', 'DashboardController@dashboard')
-         ->name('dashboard');
+Route::get('/reset', 'DashboardController@resetProjects')
+     ->name('reset');
 
-    Route::get('/reset', 'DashboardController@resetProjects')
-         ->name('reset');
-});
 
 Route::post('/create-project', 'DashboardController@createProject')
      ->name('create-project');
